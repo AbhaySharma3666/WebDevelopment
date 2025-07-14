d09a60fd877c9d8879c847f9d43568f873a4de82
Abhay Sharma <154610193+AbhaySharma3666@users.noreply.github.com>
2025-07-14 09:36:15 +0530

    Add files via upload

---
+++ _03_JS/04-Loop/06_for of.js
@@ -0,0 +1,19 @@
+// syntax
+// for (let value of array) {
+//     // code to execute
+// }
+
+let arr = [1,2,3,4,5,6,7,8,9,10];
+
+
+for(let value of arr){
+    console.log(value);
+}
+
+let arr1 = 'abhay';
+
+for(let value of arr1){
+    console.log(value);
+}
+
+
---
+++ _03_JS/04-Loop/4_forEach loop.js
@@ -0,0 +1,14 @@
+// syntax
+// array.forEach(function(element, index, array) {
+//     // code to execute
+// });
+
+
+let arr = [1,2,3,4,5,6,7,8,9,10];
+
+
+arr.forEach((value, index) => {
+    console.log('Number',value, 'Index', index);
+});
+
+
