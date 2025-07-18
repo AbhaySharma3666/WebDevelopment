382c046453e54558e846862f8b273bf1540976e7
Abhay Sharma <154610193+AbhaySharma3666@users.noreply.github.com>
2025-07-18 09:38:59 +0530

    function

---
+++ _03_JS/06-Function/Return_function.js
@@ -0,0 +1,20 @@
+function myspace(firstName, lastName){
+    let fullName = firstName + " " + lastName;
+    return fullName;
+
+    //unreachable code
+    let a = 10;
+    let b = 20;
+    let c = a + b;
+    console.log(c);
+}
+
+let ans = myspace("Abhay", "Sharma");
+console.log(ans);
+
+
+
+
+
+
+
