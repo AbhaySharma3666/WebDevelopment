7cf029ebbbfbed710445cb45726a1cf957f454f2
Abhay Sharma <154610193+AbhaySharma3666@users.noreply.github.com>
2025-07-22 09:04:09 +0530

    Add files via upload

---
+++ _03_JS/08-Array/06 Sort.js
@@ -0,0 +1,31 @@
+let arr1 = [8,7,6,9,45,53,2,1,23,89];
+
+arr1.sort();
+console.log('Array after sort :',arr1);
+
+
+
+
+let arr = [4,54,232,7,45,7,46,73456,453,45,5,4,534,6,76,6];
+
+arr.sort((a, b) => b - a);
+console.log('Array after sort :',arr);
+
+
+console.log(arr1.indexOf(89)); // return index of element
+
+let arr2 = [1,2,3,4,5,6,7,8,9,10];
+let findElement = arr2.find((element) => {
+   return element > 5;
+}); 
+console.log('Find element greater than 5:', findElement); // returns first element that satisfies condition
+
+console.log();
+
+let findIndex = arr2.findIndex((element) => {
+   return element > 5;
+});
+console.log('Find index of element greater than 5:', findIndex); // returns index of first element that satisfies condition
+
+
+
