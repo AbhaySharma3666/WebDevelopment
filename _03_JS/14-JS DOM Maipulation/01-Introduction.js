1618fa37ea2ecc79329f4a82702bfbdbeab4ccca
Abhay Sharma <154610193+AbhaySharma3666@users.noreply.github.com>
2025-07-26 09:49:24 +0530

    Error handling and DOM Maipulation

---
+++ _03_JS/14-JS DOM Maipulation/01-Introduction.js
@@ -0,0 +1,10 @@
+/*  Document Object Model (DOM)
+        The Document Object Model (DOM) is a programming interface for web documents, providing a structured representation of HTML and XML documents. It allows JavaScript to interact with and manipulate the content, structure, and style of web pages dynamically.
+
+    Browser Object Model (BOM)
+        The Browser Object Model (BOM) provides JavaScript interfaces for interacting with the browser window and its components, such as the location, history, and navigator. It's not standardized like the DOM and varies across different browsers.    
+
+    Browser Object Model (BOM)
+        Browser Object Model (BOM) provides JavaScript interfaces for interacting with the browser window and its components, such as the location, history, and navigator. It's not standardized like the DOM and varies across different browsers.
+
+*/
---
+++ _03_JS/14-JS DOM Maipulation/02-Accessing DOM.js
@@ -0,0 +1,37 @@
+/*
+    Accessing DOM Elements
+        Accessing DOM (Document Object Model) elements is a fundamental aspect of web development, enabling us to interact with and manipulate the structure of web pages dynamically.
+        access by id, class, tag name, query selector, and more.
+*/
+
+
+// 1. Accessing by ID
+    // Syntax: document.getElementById('id')
+
+    document.getElementById("codehelp");
+
+// 2. Accessing by Class Name
+    // Syntax: document.getElementsByClassName('className')
+
+    document.getElementsByClassName("code");
+
+// 3. Accessing by Tag Name
+    // Syntax: document.getElementsByTagName('tagName')
+
+    document.getElementsByTagName("h1");
+    
+// 4. Accessing by Query Selector
+    // Syntax: document.querySelector('selector')
+
+    document.querySelector("#codehelp"); // Accessing by ID
+    document.querySelector(".code"); // Accessing by Class
+    document.querySelector("h1"); // Accessing by Tag Name
+    document.querySelector("div > h1"); // Accessing by CSS Selector    
+
+// 5. Accessing by Query Selector All
+    // Syntax: document.querySelectorAll('selector')
+
+    document.querySelectorAll(".code"); // Accessing by Class
+    document.querySelectorAll("h1"); // Accessing by Tag Name
+    document.querySelectorAll("div > h1"); // Accessing by CSS Selector
+    document.querySelectorAll("#codehelp"); // Accessing by ID
