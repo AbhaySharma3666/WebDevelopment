91c0c5cab3f36e447d8aff06b3ab48106ad29c65
Abhay Sharma <154610193+AbhaySharma3666@users.noreply.github.com>
2025-08-15 13:25:12 +0530

    Various Event

---
+++ _05_JQuery/_02_Various_Event/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+    var x = $(".mydiv1").html();
+    console.log(x);
+
+    // Event on click
+    $(".btn").click(function () {
+        alert("hello, You click me !!...")
+    })
+
+    // on one click
+    $(".mydiv1").click(function () {
+        $(this).css("color", "red");
+        $(".sp1").css("color", "green");
+        $(".sp2").css("color", "red");
+        $(".sp3").css("color", "red");
+        $(this).css("background-color", "black");
+        $(this).css("padding", "10px");
+    });
+
+    // on double-click
+    $(".mydiv1").dblclick(function () {
+        $("h2").css("color", "green");
+        $(".sp1").css("color", "red");
+        $(".sp2").css("color", "green");
+        $(".sp3").css("color", "red");
+        $(this).css("border", "5px solid blue");
+        $(this).css("border-radius", "10px");
+        $(this).css("padding", "10px");
+    });
+
+    // on right click
+    $(".mydiv1").contextmenu(function () {
+        $("h2").css("color", "green");
+        $(".sp1").css("color", "black");
+        $(".sp2").css("color", "black");
+        $(".sp3").css("color", "green");
+        $(this).css("border", "5px solid blue");
+        $(this).css("border-radius", "10px");
+        $(this).css("padding", "10px");
+        $(this).css("width", "250px");
+    });
+
+    // on hover 
+    $(".mydiv1").mouseenter(function () {
+        $(this).css("background-color", "cyan");
+        $(this).css('width' , " 550px");
+        $(this).css("border", "5px dotted red");
+    });
+    // leave hover
+    $(".mydiv1").mouseleave(function () {
+        $(this).css("background-color", "");
+        $(this).css('width' , "100%");
+        $(this).css("border", "");
+    });
+    // any key press
+    $("body").keypress(function () {
+        $(this).css("background-color", "grey");
+    });
+})
