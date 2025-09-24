6ef262c7f61d4f4902d22759343b0a86b5e7e5a7
Abhay Sharma <154610193+AbhaySharma3666@users.noreply.github.com>
2025-09-24 13:14:29 +0530

    Delete _11_Spring_Boot_Framework(STS-Spring Tool Suite) directory

--- _11_Spring_Boot_Framework(STS-Spring Tool Suite)/_13_Spring_STS_03_JPA/src/main/java/com/springSTS/StudentRepo.java
+++
@@ -1,8 +0,0 @@
-package com.springSTS;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepo extends CrudRepository<Student, Integer>{
-	
-	
-}
--- _11_Spring_Boot_Framework(STS-Spring Tool Suite)/_17_Spring_RestAPI_DB/src/main/java/com/springSTS/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.springSTS;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepo extends CrudRepository<Employee, Integer>{
-	
-	public Employee findById(int id); 
-
-}
