ad4737b365047876e5c2dab8f4d75117c79418f7
Abhay Sharma <154610193+AbhaySharma3666@users.noreply.github.com>
2025-10-26 08:57:18 +0530

    hibernation

---
+++ _09_Hibernate_(Advanced_Java_Framework)_in_eclipse/_17_Hibernate_Proj1/src/main/java/hibernate/pack/App.java
@@ -0,0 +1,37 @@
+package hibernate.pack;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    		Student so1 = new Student();
+    		so1.setS_id(1);
+    		so1.setS_name("Abhay");
+    		so1.setCity("Bhilai");
+    	
+        Configuration cfg = new Configuration();
+        cfg.configure("/configuration/pack/hibernate.cfg.xml");
+        
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session s = sf.openSession();
+        Transaction tr = s.beginTransaction();
+        
+        
+        try {
+        		s.save(so1);
+        		tr.commit();
+        }catch (Exception e) {
+			tr.rollback();
+			e.printStackTrace();
+		}
+    }
+}
